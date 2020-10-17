899b9cb746d1d42b3c786acb668e3ce6707216af
liangt2001 <64676038+liangt2001@users.noreply.github.com>
2020-10-17 19:05:26 -0400

    joined the csv files

--- main.py
+++ main.py
@@ -2,4 +2,16 @@ import streamlit as st
 import pandas as pd
 
 citiesCSV = pd.read_csv('cities.csv')
-st.write(citiesCSV['City'])
+colCSV = pd.read_csv('movehubcostofliving.csv')
+qolCSV = pd.read_csv('movehubqualityoflife.csv')
+priceCSV = pd.read_csv('price.csv')
+pricePerSqftCSV = pd.read_csv('pricepersqft.csv')
+
+# dt = colCSV.set_index('City').join(citiesCSV.set_index('City'))
+
+colCSV = colCSV.set_index('City')
+citiesCSV = citiesCSV.set_index('City')
+result = colCSV.join(citiesCSV)
+result = result.dropna()
+# result = pd.concat([colCSV, citiesCSV], axis=1, join='inner')
+st.write(result)
