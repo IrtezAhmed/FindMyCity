9c1084788532479270625f45d522c244c0b52fa0
IrtezaAhmed <61393006+IrtezaAhmed@users.noreply.github.com>
2020-10-17 19:06:12 -0400

    updates

---
+++ aggregate.py
@@ -0,0 +1,4 @@
+#aggregating data from different cities into one so that is is accesible by the clustering algorithm
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import streamlit as st
 import pandas as pd
 
-st.write("Take Me Home, Country Roads?")
+st.write("**Take Me Home, Country Roads**")
 
+st.write("John Denver's words **impacted millions**. Now it's time for it to impact you - where is your home, country roads?")
+
+st.write("With our app, you can find just that.")
+
+#reading all CSV files
+cities = pd.read_csv('cities.csv')
+CoL = pd.read_csv('movehubcostofliving.csv')
+QoL = pd.read_csv('movehubqualityoflife.csv')
+prices = pd.read_csv('price.csv')
+pricesSQFT = pd.read_csv('pricepersqft.csv')
+
+
+
+
+st.write(cities)
+st.write(CoL)
+st.write(QoL)
+st.write(prices)
+st.write(pricesSQFT)
