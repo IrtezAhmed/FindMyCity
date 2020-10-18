03dfef55c45017a22fd1a8ea407a7104ff974334
Zamzam95-Coder <zam19@rogers.com>
2020-10-18 00:01:26 -0400

    test

--- main.py
+++ main.py
@@ -26,14 +26,14 @@ userLocation = st.selectbox("Where do you live right now?", rows)
 #st.write(userLocation)
 userDistance = st.slider("How far are you willing to move away?", 0, 10000, 0, 200)
 userPop = st.slider("How populated do you want this city to be? (1 = Sparse, 5 = Crowded)", 1, 5, 1)
-userRent = st.slider("How much rent are you willing to pay? (Specify a range)", 600, 6000, (2000, 4000), 100)
+userRent = st.slider("How much rent are you willing to pay? (Specify a range)", 600, 22000, (3000, 6000), 100)
 userJob = st.slider("How important is the local job market for you? (1 = Not important, 5 = Very important", 1, 5, 1)
 userClimate = st.slider("What kind of climate do you prefer? (1 = Cold, 5 = Hot)", 1, 5, 1)
 
 st.write(userRent[0])
 st.write(userRent[1])
 
-fig = px.scatter_mapbox(result, lat="lat", lon="lng", color="Movehub Rating", hover_name=rows, size="Quality of Life", 
+fig = px.scatter_mapbox(result, lat="lat", lon="lng", color="Movehub Rating", hover_name=rows, hover_data=[range(len(rows)),range(len(rows))], size="Quality of Life", 
                         color_continuous_scale=px.colors.diverging.RdYlGn, zoom=1, mapbox_style="carto-positron", size_max=15)
 
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
