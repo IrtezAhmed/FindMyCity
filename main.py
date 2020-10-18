86331117f4452ce3d8e1c921cca25da88084b909
IrtezaAhmed <61393006+IrtezaAhmed@users.noreply.github.com>
2020-10-17 21:09:22 -0400

    fixed

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ st.write("John Denver's words **impacted millions**. Now it's time for it to imp
 st.write("With our app, you can find just that.")
 
 #reading all CSV files
+citiesRef = pd.read_csv('cities.csv')
 cities = pd.read_csv('cities.csv').set_index('City')
 CoL = pd.read_csv('movehubcostofliving.csv').set_index('City')
 QoL = pd.read_csv('movehubqualityoflife.csv').set_index('City')
 
-result = QoL.join(cities).join(CoL)
+result = cities.join(QoL).join(CoL)
+
+result = result.dropna()
 
 st.write(result)
 
-fig = px.scatter_mapbox(result, lat="lat", lon="lng", hover_name="Country", hover_data=["Gasoline", "Cinema"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
-                        
+rows = list(result.index)
+
+fig = px.scatter_mapbox(result, lat="lat", lon="lng", color="Movehub Rating", hover_name=rows, size="Quality of Life", 
+                        color_continuous_scale=px.colors.diverging.RdYlGn, zoom=1, mapbox_style="carto-positron", size_max=15)
+
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 st.plotly_chart(fig)
