1a2d112e2cfebe719041855cfb44165c2b60941c
IrtezaAhmed <61393006+IrtezaAhmed@users.noreply.github.com>
2020-10-18 12:59:16 -0400

    klkkl

---
+++ algop.py
@@ -0,0 +1,28 @@
+#clustering algorithm
+st.write('TRAINING......')
+model = KMeans(n_clusters = 5, n_init=5, init='random')
+model.fit(resultFinal)
+#labels = model.predict(resultFinal)
+pickle.dump(model, open('mode.sav', 'wb'))
+st.write("TRAINING COMPLETE.")
+
+np.save('label.npy', labels)
+st.write('TRAINING COMPLETE')
+clusterVal = pd.DataFrame(labels, columns=['Cluster'], index = rows) #creates a DF of cities to their respective clusters
+
+#creates dictionary values that are easily accessible
+clusterDict = {}
+n = 0
+for clusterNum in labels:
+    clusterNum = str(clusterNum)
+    if clusterNum in clusterDict:
+        clusterDict[clusterNum].append(rows[n])
+    else:
+        clusterDict[clusterNum] = [rows[n]]
+    n+=1
+
+np.save('dictionary.npy', clusterDict)
+
+
+
+
