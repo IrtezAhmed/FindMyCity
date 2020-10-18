8674a07a9a44eef63fc1bf30dc0555e9772baa90
Zamzam95-Coder <zam19@rogers.com>
2020-10-17 23:13:36 -0400

    web app

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
-
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-citiesCSV = pd.read_csv('cities.csv')
-colCSV = pd.read_csv('movehubcostofliving.csv')
-qolCSV = pd.read_csv('movehubqualityoflife.csv')
-priceCSV = pd.read_csv('price.csv')
-pricePerSqftCSV = pd.read_csv('pricepersqft.csv')
+st.title("Find My City")
+#st.write("**Take Me Home, Country Roads**")
 
-# dt = colCSV.set_index('City').join(citiesCSV.set_index('City'))
+#st.write("John Denver's words **impacted millions**. Now it's time for it to impact you - where is your home, country roads?")
 
-colCSV = colCSV.set_index('City')
-citiesCSV = citiesCSV.set_index('City')
-result = colCSV.join(citiesCSV)
-result = result.join(qolCSV)
+#st.write("With our app, you can find just that.")
+
+#reading all CSV files
+citiesRef = pd.read_csv('cities.csv')
+cities = pd.read_csv('cities.csv').set_index('City')
+CoL = pd.read_csv('movehubcostofliving.csv').set_index('City')
+QoL = pd.read_csv('movehubqualityoflife.csv').set_index('City')
+
+result = cities.join(QoL).join(CoL)
+
 result = result.dropna()
-# result = pd.concat([colCSV, citiesCSV], axis=1, join='inner')
-st.write(result)
 
-fig = px.scatter_mapbox(result, lat="lat", lon="lng", hover_name="Country", hover_data=["Gasoline", "Cinema"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+rows = list(result.index)
+
+userLocation = st.selectbox("Where do you live right now?", rows)
+#st.write(userLocation)
+userDistance = st.slider("How far are you willing to move away?", 0, 10000, 0, 200)
+userPop = st.slider("How populated do you want this city to be? (1 = Sparse, 5 = Crowded)", 1, 5, 1)
+userRent = st.slider("How much rent are you willing to pay? (Specify a range)", 600, 6000, (2000, 4000), 100)
+userJob = st.slider("How important is the local job market for you? (1 = Not important, 5 = Very important", 1, 5, 1)
+userClimate = st.slider("What kind of climate do you prefer? (1 = Cold, 5 = Hot)", 1, 5, 1)
+
+st.write(userRent[0])
+st.write(userRent[1])
+
+fig = px.scatter_mapbox(result, lat="lat", lon="lng", color="Movehub Rating", hover_name=rows, size="Quality of Life", 
+                        color_continuous_scale=px.colors.diverging.RdYlGn, zoom=1, mapbox_style="carto-positron", size_max=15)
+
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
 st.plotly_chart(fig)
